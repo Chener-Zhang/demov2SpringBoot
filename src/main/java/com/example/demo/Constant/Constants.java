7158fb1aeef24ab254ad1ad2014767713ec79943
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-11-16 20:19:37 -0800

    Initial commit

---
+++ src/main/java/com/example/demo/Constant/Constants.java
@@ -0,0 +1,7 @@
+package com.example.demo.Constant;
+
+public interface Constants {
+
+    public final static String PI_VALUE = "3.14";
+    public final static String DATE_TIME_FORMAT = "dd/MM/YYYY";
+}
